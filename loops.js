4246f37b9ac394d67e13377cfdce4e8f4954cd36
Nate Nelson <7nater.nelson@gmail.com>
2020-03-31 17:07:09 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,30 +1,10 @@
-function forLoop([array]){
-  var array = [];
-  for (let i = 0; i < 25; i++) {
-    if(i===0){
-      array.push(“I am 1 strange loop.”);
-    } else {
-      array.push(“I am ${i} strange loop.”);
+function forLoop(array){
+  for(let i = 0; i < 25; i++){
+      if(i==0){
+        array.push = "I am 1 strange loop."
+      } else {
+        array.push = "I am " i "strange loops."
       }
-    }
-    return array;
-}
-
-
-function whileLoop(number) {
-while (number > 0) {
-console.log(–number);
-}
-return “done”;
-}
-function doWhileLoop(array){
-function maybeTrue() {
-return Math.random() >= 0.5
-}
-
-do {
-array.pop();
-} while (array.length > 0 &&maybeTrue());
-return array;
-
+  }
+  return array;
 }
